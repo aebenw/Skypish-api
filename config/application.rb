3704afec17eeb2ecc459f102734fb082d868954f
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-20 22:59:01 -0400

    trying another thing

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,13 @@ module SkypeWannaBe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000', 'https://hidden-fortress-58788.herokuapp.com/' #replace this url with that of your own heroku client app
+    resource '*', :headers => :any, :methods => [:get]
+  end
+end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
