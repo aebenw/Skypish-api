f8434af9227abfd987d1507215edd000bed9f18b
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-20 22:32:41 -0400

    attempt 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :create]
   resources :messages, only: [:create]
   mount ActionCable.server => '/cable'
-
-  root 'conversations#index'
   #lets see if this works now
 end
