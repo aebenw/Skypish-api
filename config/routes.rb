563994ff37c2a315bc71ec1304b327ad21873b4e
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-20 22:20:21 -0400

    set up a default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :create]
   resources :messages, only: [:create]
   mount ActionCable.server => '/cable'
+
+  root 'conversations#index'
 end
