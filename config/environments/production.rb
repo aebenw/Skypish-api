8fed79cabf36a421fc6be9df777b59bd1f0f03ae
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-22 11:02:25 -0400

    changed production and development for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'ws://2cc7b21c.ngrok.io/cable'
-  # config.action_cable.allowed_request_origins = '*'
+  config.action_cable.url = 'ws://localhost:3001/cable'
+  config.action_cable.allowed_request_origins = '*'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
